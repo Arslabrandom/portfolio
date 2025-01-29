12103eb003d426026873c6fd0212c2c49825e4d4
Arslabrandom <158280913+Arslabrandom@users.noreply.github.com>
2025-01-29 21:11:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/src', express.static('src'));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/blogs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'apology.html'));
+})
+
+app.get('/hireme', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'hireme.html'));
+})
+
+app.get('/downloadcv', (req, res) => {
+    const filePath = path.join(__dirname, 'src', 'cv.txt');
+    res.download(filePath, 'cv.txt', (err) => {
+        if (err) {
+            console.error('Error downloading file:', err);
+            res.status(500).send('Error downloading file');
+        }
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://192.168.234.139:${PORT}/`);
+});
---
+++ script.js
@@ -0,0 +1,27 @@
+const homebtn = document.querySelectorAll('.red-home');
+const funcBtns = document.querySelectorAll('.functionbtn');
+
+Array.from(funcBtns, (btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.value == 'dwnldcd') {
+            downloadCV();
+        };
+        if (btn.value == 'hireme') {
+            hireme();
+        };
+    });
+});
+
+Array.from(homebtn, (btn) => {
+    btn.addEventListener('click', () => {
+        window.location.href = "http://192.168.234.139:3000/";
+    });
+});
+
+function downloadCV() {
+    window.location.href = 'http://192.168.234.139:3000/downloadcv';
+};
+
+function hireme() {
+    window.location.href = "http://192.168.234.139:3000/hireme";
+};
